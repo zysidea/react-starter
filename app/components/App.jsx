b2faba798698d7887f5b78e95e72dd9be929df94
zysidea <zysidea@gmail.com>
2017-08-01 11:09:51 +0800

    增加类型检测

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import ProTypes from 'prop-types';
 import './App.css';
 
 export default class Counter extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
   }
 
   render() {
@@ -16,3 +16,7 @@ export default class Counter extends React.Component {
     );
   }
 }
+Counter.propTypes = {
+  value: ProTypes.string.isRequired
+}
+
